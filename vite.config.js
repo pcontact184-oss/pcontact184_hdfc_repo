f4ded447e1607b22dc71e80ff17298531c34f5bd
pcontact184-oss <YOUR_EMAIL_USED_IN_THIS_ACCOUNT>
2026-03-27 00:09:24 +0530

    commit1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 ﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5174,
-    strictPort: true
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' && process.env.GITHUB_ACTIONS ? '/pcontact184_hdfc_repo/' : '/',
+    plugins: [react()],
+    server: {
+      port: 5174,
+      strictPort: true
+    }
   }
 })
